8ae24a8a91458a2b8bb3062bf0975d056433cf14
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-04 21:43:05 -0800

    Initial commit, file structure and files. TODO: plenty

---
+++ client/client.js
@@ -0,0 +1,45 @@
+const net = require('net');
+const readline = require('readline');
+let connection;
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost', // change to IP address of computer or ngrok host if tunneling
+    port: 3000 // or change to the ngrok port if tunneling
+  });
+  // interpret data as text
+  conn.setEncoding('utf8'); 
+  // On connect:
+  conn.on('connect', () => {
+    console.log('Successful connection')
+  });
+  // When data received:
+  conn.on('data', (data) => {
+    console.log('From server:::', data);
+  });
+
+  return conn;
+}
+
+
+const userInput = (connection) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  rl.question('Filename:::', (answer) => {
+    // console.log(`Your answer: ${answer}`);
+    connection.write(answer);
+    rl.close();
+    process.exit()
+  });
+}
+
+userInput(connect())
+// connect()
+
+module.exports = { connect }
+
+
+
---
+++ server/server.js
@@ -0,0 +1,42 @@
+
+
+/*
+  Build a file server and client where:
+    Clients can connect to the server via TCP and send a request for a file (by filename)
+    The server looks for requested files locally and sends back the data
+*/
+
+const net = require('net');
+const server = net.createServer();
+
+const launchServer = function() {
+  server.on('connection', (client) => {
+    console.log('New client connected');
+    // client.write('Hello there!');
+    
+    client.setEncoding('utf-8');
+    client.on('data', (data) => {
+      // Client has sent a filename to look for
+      client.write('you sent me:::' + data)
+      console.log('Message from client:::', data)
+    })
+  });
+  
+  server.listen(3000, () => {
+    console.log('Server listening on port 3000!');
+  });
+
+  // server.on('data', (data) => {
+
+  // })
+}
+
+// const readFile = function(path) {
+
+//   return "file contents placeholder"
+// }
+
+
+launchServer()
+  
+  
